805bfa57773b45724891bf4671a3143a702e8772
Julianquin <julian.alberto.quintero@correounivalle.edu.co>
2019-12-25 17:20:58 -0500

    primer commit

---
+++ integracion.r
@@ -0,0 +1,8 @@
+# Intalar Git en el pc
+# Abrir Git e ingresar con el correo y el usuario
+
+git config --global user.email "julian.alberto.quintero@correounivalle.edu.co"
+git config --global user.name "Julianquin"
+
+# Buscar el repositorio desde la web
+
