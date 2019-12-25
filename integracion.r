55e595f97469fa8a5f99baf28fbce28f0d4f2f59
Julianquin <julian.alberto.quintero@correounivalle.edu.co>
2019-12-25 17:25:25 -0500

    actualzacion

--- integracion.r
+++ integracion.r
@@ -1,8 +1,11 @@
+# Ver video en https://www.youtube.com/watch?v=qcjpHFwCugE
 # Intalar Git en el pc
 # Abrir Git e ingresar con el correo y el usuario
 
 git config --global user.email "julian.alberto.quintero@correounivalle.edu.co"
 git config --global user.name "Julianquin"
+
+
 
 # Buscar el repositorio desde la web
 
